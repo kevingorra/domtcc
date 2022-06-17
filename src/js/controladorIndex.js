88ccfffba425b6fbf623ce8048593717c1b5bced
Sebastian Patiño <sebastianpa138@gmail.com>
2022-06-17 12:10:01 -0500

    clase arreglos y objetos

--- src/js/controladorIndex.js
+++ src/js/controladorIndex.js
@@ -1,9 +1,387 @@
-//ENTRADAS == VARIABLES
-var nombre="JAMES RODRIGUEZ"
+// let nombreProductoMedellin = 'funko pop de petro'
+// let precioProducto = 45000
+// let cantidadProducto = 5
+// let imagenProducto = 'na'
+// let identificacionUsuario = '1040759578'
+// const IVA = 0.19
+// const PAIS_USUARIO = 'col'
 
-if(true){
-    var nombre="Lucho diaz"
+// construyendo arreglos
+let numeros = [1,2,3,4,5]
+let nombres = ['juan','camila','andres','catalina']
+let votos = [true,true,false,false,false,true]
+// forma tradicional
+
+// forma poo
+let numerosPOO = Array(1,2,3,4,5)
+let nombresPOO = Array('juan','camila','andres','catalina')
+let votosPOO = Array (true,true,false,false,false,true)
+
+//imprimir los elementos del arreglo
+console.log(numeros)
+console.log(numerosPOO)
+
+console.log(numeros[3])
+
+//objetos en js
+
+let persona ={
+    nombre:'kevin',
+    edad: 22,
+    hinchaDelMedellin:true,
+    foto:'url'
 }
-console.log(`hola ${nombre}`)
-console.log("hola")
-console.log("adios")
+
+console.log(persona)
+console.log(persona.nombre)
+
+let producto1 ={
+    nombre:'camisa Nike',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado Nike'
+}
+let producto2 ={
+    nombre:'camisa Adidas',
+    precio:35000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado Adidas'
+}
+let producto3 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto4 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto5 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto6 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto7 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto8 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto9 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto10 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto11 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto12 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto13 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto14 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto15 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto16 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto17 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto18 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto19 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto20 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto21 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto22 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto23 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto24 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto25 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto26 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto27 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto28 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto29 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto30 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto31 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto32 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto33 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto34 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto35 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto36 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto37 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto38 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto39 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto40 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto41 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto42 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto43 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto44 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto45 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto46 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto47 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto48 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto49 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
+let producto50 ={
+    nombre:'camisa',
+    precio:30000,
+    cantidad:1,
+    foto:'url',
+    descripcion: 'camisa con estampado nike'
+}
